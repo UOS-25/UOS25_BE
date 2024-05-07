cdb04647ecce86b3ac59cfb5709b46b3a01624f1
gosu <q63530@naver.com>
2024-05-07 17:28:38 +0900

    initial commit

---
+++ src/main/java/com/uos25/uos25/Uos25Application.java
@@ -0,0 +1,13 @@
+package com.uos25.uos25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uos25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uos25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uos25/uos25/Uos25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uos25.uos25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uos25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
