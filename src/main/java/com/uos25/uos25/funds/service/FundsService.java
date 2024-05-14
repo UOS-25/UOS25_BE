3f767a31e0362ca2543b2ca18af20c8f1444e95f
gosu <q63530@naver.com>
2024-05-14 13:49:05 +0900

    [feat] : sales 기능 구현

--- src/main/java/com/uos25/uos25/funds/service/FundsService.java
+++ src/main/java/com/uos25/uos25/funds/service/FundsService.java
@@ -1,8 +1,6 @@
 package com.uos25.uos25.funds.service;
 
 import com.uos25.uos25.common.error.funds.FundsNotFoundException;
-import com.uos25.uos25.funds.dto.FundsDTO.MaintenanceDecideRequest;
-import com.uos25.uos25.funds.dto.FundsDTO.WithdrawalRequest;
 import com.uos25.uos25.funds.entity.Funds;
 import com.uos25.uos25.funds.repository.FundsRepository;
 import com.uos25.uos25.store.service.StoreService;
@@ -18,10 +16,8 @@ public class FundsService {
     private final FundsRepository fundsRepository;
     private final StoreService storeService;
 
-    @Transactional
-    public void withdrawal(WithdrawalRequest request, Long storeId) {
-        int money = request.getMoney();
-
+    @Transactional //출금
+    public void withdrawal(int money, Long storeId) {
         Funds funds = fundsRepository.findByStoreId(storeId).orElseThrow(
                 FundsNotFoundException::new
         );
@@ -32,14 +28,23 @@ public class FundsService {
     }
 
     @Transactional //유지비 결정
-    public void decideMaintenance(MaintenanceDecideRequest request, Long storeId) {
-        int expense = request.getExpense();
-
+    public void decideMaintenance(int expense, Long storeId) {
         Funds funds = fundsRepository.findByStoreId(storeId).orElseThrow(
                 FundsNotFoundException::new
         );
 
         funds.decideMaintenanceExpense(expense);
+
+        fundsRepository.save(funds);
+    }
+
+    @Transactional //유지비 결정
+    public void sales(int money, Long storeId) {
+        Funds funds = fundsRepository.findByStoreId(storeId).orElseThrow(
+                FundsNotFoundException::new
+        );
+
+        funds.updateSales(money);
 
         fundsRepository.save(funds);
     }
